5a2c13a4e8ad25afb885a123746eee2970d3658d
Srikanth Parsi <parsi.srikanth218@gmail.com>
2026-08-23 16:27:21 +0000

    feat: add Word Race game rules

---
+++ src/games/word-race/index.ts
@@ -0,0 +1,10 @@
+export { markWordleGuess, marksToEmoji } from "./feedback";
+export type { LetterMark } from "./feedback";
+export { createWordRaceGame, WORD_RACE_MAX_GUESSES, wordRace } from "./game";
+export type {
+  WordRaceCommand,
+  WordRaceGuess,
+  WordRaceOpponentProgress,
+  WordRacePublicState,
+  WordRaceState,
+} from "./game";
